a754749d34a48fab5bf05edf08c20237f1b1de7b
luongnguyhen <nguyenluong24052002@gmail.com>
2023-06-27 09:34:15 +0700

    admin-management1

--- Admin-management/products/list.php
+++ Admin-management/products/list.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
 <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
     <h2 class="display-4">Product Management</h2>
     <p><a href="index.php?module=product&action=create">Add new</a></p>
@@ -28,7 +19,7 @@
             <th scope="row">1</th>
             <td>Mark</td>
             <td>Otto</td>
-            <td><a href="index.php?module=product&action=edit">Edit</a></td>
+            <td><a href="index.php?module=product&action=create">Edit</a></td>
           </tr>
           <tr>
             <th scope="row">2</th>
@@ -46,5 +37,3 @@
       </table>
     </div>
   </div>
-</body>
-</html>
--- Admin-management/user/create.php
+++ Admin-management/user/create.php
@@ -1,27 +1,6 @@
-<html lang="en"><head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <link rel="icon" href="/docs/4.1/assets/img/favicons/favicon.ico">
-  <title>suntech.edu.vn</title>
-  <link rel="canonical" href="https://getbootstrap.com/docs/4.1/examples/pricing/">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" >
-</head>
-
-<body>
-  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-    <h5 class="my-0 mr-md-auto font-weight-normal">Vue Mini Project</h5>
-    <nav class="my-2 my-md-0 mr-md-3">
-      <a class="p-2 text-dark" href="#">Product management</a>
-      <a class="p-2 text-dark" href="#">User management</a>
-    </nav>
-    <a class="btn btn-outline-primary" routerlink="cart">Administrator</a>
-  </div>
-
 <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
     <h3 class="display-5">Product Infomation</h3>
-    <router-link to="/about">Back</router-link>
+    <a href="index.php?module=product&action=list">Back</a>
 </div>
 
 <div class="container">
@@ -53,9 +32,3 @@
             </div>
         </div>
     </form>
-</div>
-
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-</body>
-</html>
